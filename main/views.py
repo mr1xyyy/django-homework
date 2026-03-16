bce4f453b46921c440f8d31f4083d2d056c5f4a4
mr1xyyy <oybeknarimonov@gmail.com>
2026-03-16 23:26:29 +0500

    remove pycache

--- main/views.py
+++ main/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request,'main/index.html')
+    data = {
+        'title': 'Bosh sahifa',
+    }
+    return render(request,'main/index.html', data)
 
 def about(request):
     return render(request,'main/about.html')
---
+++ news/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField('Nomi', max_length=255)
+    anons = models.CharField('Anons', max_length=255)
+    full_text = models.TextField('Maqola')
+    date = models.DateTimeField('Nashr etilgan sana')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Yangilik'
+        verbose_name_plural = 'Yangiliklar'
