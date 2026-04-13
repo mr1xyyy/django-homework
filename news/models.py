1d1be480eb625e0347cfd9e2b7179d17c595bb6e
mr1xyyy <oybeknarimonov@gmail.com>
2026-04-13 14:50:55 +0500

    update

--- news/models.py
+++ news/models.py
@@ -9,6 +9,8 @@ class News(models.Model):
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        return f'/news/{self.id}'    
     class Meta:
         verbose_name = 'Yangilik'
         verbose_name_plural = 'Yangiliklar'
--- news/views.py
+++ news/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import News
 from .forms import ArticleForm 
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news_home(request):
     news = News.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+class NewsDetailView(DetailView):
+    model = News
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = News
+    template_name = 'news/create.html'
+    
+    form_class = ArticleForm
+
+class NewsDeleteView(DeleteView):
+    model = News
+    success_url = '/news/'
+    template_name = 'news/news_delete.html'
+    
 
 def create(request):
     error = ''
